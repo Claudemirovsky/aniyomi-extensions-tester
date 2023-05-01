7079a386603f3b928d8c35e28d2b76f6784a4f19
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-05-01 02:10:57 -0300

    chore(release): prepare for new release

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -51,7 +51,7 @@ sourceSets {
 }
 
 // should be bumped with each stable release
-val inspectorVersion = "v2.2.0"
+val inspectorVersion = "v2.3.0"
 
 // counts commit count on master
 val inspectorRevision = runCatching {
