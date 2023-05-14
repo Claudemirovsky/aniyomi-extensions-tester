0df3131f2981f92150d8a727bc97e9b550ec789a
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-05-14 10:51:29 -0300

    refactor(settings.build.kts): Remove unnecessary repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,11 +7,8 @@ dependencyResolutionManagement {
 
     repositories {
         mavenCentral()
-        maven("https://maven.google.com/")
+        google()
         maven("https://jitpack.io")
-        maven("https://dl.google.com/dl/android/maven2/")
-        maven("https://repo1.maven.org/maven2/")
-        maven("https://oss.sonatype.org/content/repositories/snapshots/")
         maven("https://github.com/Claudemirovsky/aniyomi-extensions-tester/raw/android-jar/")
     }
 }
