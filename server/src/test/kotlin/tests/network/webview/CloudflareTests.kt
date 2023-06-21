23def18450ac122ac4906a859674f6a02f18c673
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-06-20 22:00:47 -0300

    fix(tests/Cloudflaretests): Remove previously-added cf-clearance cookies

--- server/src/test/kotlin/tests/network/webview/CloudflareTests.kt
+++ server/src/test/kotlin/tests/network/webview/CloudflareTests.kt
@@ -5,6 +5,7 @@ import eu.kanade.tachiyomi.network.NetworkHelper
 import eu.kanade.tachiyomi.util.asJsoup
 import tests.util.AnitesterTest
 import uy.kohesive.injekt.injectLazy
+import java.net.URI
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
@@ -12,9 +13,14 @@ class CloudflareTests : AnitesterTest() {
     private val network: NetworkHelper by injectLazy()
 
     @Test fun `Is passing Cloudflare IUAM protection`() {
-        val request = GET("https://nowsecure.nl/#relax")
+        network.cookieManager.store.remove(URI(URL))
+        val request = GET(URL)
         val response = network.cloudflareClient.newCall(request).execute()
         val doc = response.use { it.asJsoup() }
         assertNotNull(doc.selectFirst(".hystericalbg"))
+    }
+
+    companion object {
+        private const val URL = "https://nowsecure.nl/#relax"
     }
 }
