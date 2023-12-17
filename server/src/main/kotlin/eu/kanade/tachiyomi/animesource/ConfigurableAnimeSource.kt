e2fb55cc0777695638476fb1b2d66a4d161274f8
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-12-17 17:59:36 -0300

    feat: Add required functions for extlib v14

--- server/src/main/kotlin/eu/kanade/tachiyomi/animesource/ConfigurableAnimeSource.kt
+++ server/src/main/kotlin/eu/kanade/tachiyomi/animesource/ConfigurableAnimeSource.kt
@@ -1,8 +1,23 @@
 package eu.kanade.tachiyomi.animesource
 
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.preference.PreferenceScreen
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 interface ConfigurableAnimeSource : AnimeSource {
 
+    /**
+     * Gets instance of [SharedPreferences] scoped to the specific source.
+     *
+     * @since extensions-lib 14
+     */
+    fun getSourcePreferences(): SharedPreferences =
+        Injekt.get<Application>().getSharedPreferences(preferenceKey(), Context.MODE_PRIVATE)
+
     fun setupPreferenceScreen(screen: PreferenceScreen)
 }
+
+private fun ConfigurableAnimeSource.preferenceKey(): String = "source_$id"
